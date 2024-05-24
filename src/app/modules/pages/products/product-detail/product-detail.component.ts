9366466b7d8dccbc7da28c94a1881ba17a742705
Kayrat <makhmudov.kay@gmail.com>
2024-05-24 17:50:54 +0500

    .

--- src/app/modules/pages/products/product-detail/product-detail.component.ts
+++ src/app/modules/pages/products/product-detail/product-detail.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [NzButtonModule, NzInputNumberModule, FormsModule],
 })
 export class ProductDetailComponent implements OnInit {
+  quantity = 1;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
